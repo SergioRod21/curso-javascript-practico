78f2af1c227c9324ca746681e1ce2a64ae3007df
SergioRod21 <sergiorod1985@gmail.com>
2021-08-16 01:43:59 -0400

    .

--- figuras.js
+++ figuras.js
@@ -13,6 +13,8 @@ console.groupEnd();
 
 //Codigo del triangulo
 console.group('Triangulo')
+
+
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
@@ -23,28 +25,60 @@ const alturaTriangulo = 5.5;
 console.log(`La altura del triangulo es: ${alturaTriangulo}cm`)
 
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log(`El perimetro del triangulo es: ${perimetroTriangulo}cm`)
+function perimetroTriangulo(lado1, lado2, base) {
+    return result = lado1 + lado2 + base,
+        console.log(`El perimetro del triangulo es: ${result}cm`)
+}
+perimetroTriangulo(2, 5, 4)
 
-const areaTriangulo = (ladoTriangulo1 * baseTriangulo) / 2;
-console.log(`El area del triangulo es: ${areaTriangulo}cm^2`)
+
+
+
+const areaTriangulo = (lado1, base) => result = (lado1 * base) / 2;
+areaTriangulo(2, 50)
+console.log(`El area del triangulo es: ${result}cm^2`)
+
+
+
 console.groupEnd();
 
 // Circulos
 console.group('Circulos')
 
-const radioCirculo = 4;
-console.log(`El radio del circulo es ${radioCirculo}cm`)
 
-const diametroCirculo = radioCirculo * 2;
-console.log(`El diametro del circulo es ${diametroCirculo}cm`)
+
+const radioCirculo = 4;
+console.log(`El radio del circulo es ${radioCirculo}cm`);
+
+function diametroCirculo(radio) {
+    return result = radio * 2,
+        console.log(`El diametro del circulo es ${result}cm`)
+};
+diametroCirculo(4);
+
 
 const PI = Math.PI;
 console.log(`PI es: ${PI}`)
+
+
+
 
 const perimetroCirculo = diametroCirculo * PI;
 console.log(`El perimetro del circulo es: ${perimetroCirculo}cm`)
 
 
+const perimetroCirculo = (diametro) => result = diametro * Math.PI;
+perimetroCirculo(3);
+console.log(`El perimetro del circulo es: ${result}cm`)
+
+
+
 const areaCirculo = (radioCirculo * radioCirculo) * PI;
-console.log(`El area del circulo es: ${areaCirculo}cm^2`)
+
+function areaCirculo(radio) {
+    return result = radio * radio * Math.PI,
+        console.log(`El area del circulo es: ${result}cm^2`)
+}
+
+areaCirculo(10);
+
